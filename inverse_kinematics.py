e5b304b3889c28be936dfde84593af867d104177
maelscott <120776507+maelscott@users.noreply.github.com>
2024-06-06 16:23:59 +0100

    Update inverse_kinematics.py

--- inverse_kinematics.py
+++ inverse_kinematics.py
@@ -20,16 +20,16 @@ def save_position(x, y, x_limits, y_limits, filename='position.txt'):
     file.write(data)
     file.close()
 
-def get_angles(x, y, l1=15.0, l2=15.0):
+def get_angles(x, y, l1=10.0, l2=10.0):
     cos_theta2 = (x**2 + y**2 - (l1**2 + l2**2)) / (2 * l1 * l2)
     cos_theta2 = min(max(cos_theta2, -1), 1)
     theta2 = mt.acos(cos_theta2)
     theta1 = mt.atan(x / y) - mt.atan((l2 * mt.sin(theta2)) / (l1 + l2 * mt.cos(theta2)))
     theta2 = (-1) * theta2
-    theta1 = 0.5*mt.pi - theta1
+    theta1 = (mt.pi / 2) - theta1
     return theta1, theta2
     
-def inverse_kin(x, y, x_limits, y_limits, horizontal_gaze_ratio, vertical_gaze_ratio, theta1tosteps=(1/1.8), theta2tosteps=(1/1.8), l1=228.0, l2=136.5):
+def inverse_kin(x, y, x_limits, y_limits, horizontal_gaze_ratio, vertical_gaze_ratio, theta1tosteps=(1/(1.8 * (mt.pi/180))), theta2tosteps=(1/(1.8 * (mt.pi/180))), l1=10.0, l2=10.0):
     new_x = x + horizontal_gaze_ratio
     new_y = y + vertical_gaze_ratio
     if int(x) < x_limits[0] or int(x) > x_limits[1]:
@@ -40,9 +40,9 @@ def inverse_kin(x, y, x_limits, y_limits, horizontal_gaze_ratio, vertical_gaze_r
     new_theta1, new_theta2 = get_angles(new_x, new_y)
     diff_theta1 = new_theta1 - theta1
     diff_theta2 = new_theta2 - theta2
-    num_steps1 = mt.ceil(diff_theta1 * theta1tosteps)
-    num_steps2 = mt.ceil(diff_theta2 * theta2tosteps)
-    return num_steps1 / 100, num_steps2 / 100, new_x, new_y
+    num_steps1 = (int(diff_theta1 * theta1tosteps * 1000)/1000) / 250
+    num_steps2 = (int(diff_theta2 * theta2tosteps * 1000)/1000) / 250
+    return num_steps1, num_steps2, new_x, new_y
 
 if __name__ == '__main__':
     save_position(10.0, 10.0, [0, 20], [0, 20])
