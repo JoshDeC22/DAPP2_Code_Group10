109e30bcc4f4c1d3cfcfc5136094733060ac471f
maelscott <120776507+maelscott@users.noreply.github.com>
2024-06-07 11:51:51 +0100

    Update grbl_sender.py

--- grbl_sender.py
+++ grbl_sender.py
@@ -11,17 +11,18 @@ def open_grbl(port_name, baudrate):
     return s
 
 def close_grbl(serial_port):
+    serial_port.reset_input_buffer()
     serial_port.close()
     
 def send_command(voice_command, vertical_gaze_ratio, horizontal_gaze_ratio, s, is_moving):
     if is_moving == True:
         gcode = 'G21G91G1' + 'X' + str(horizontal_gaze_ratio) + 'Y' + str(vertical_gaze_ratio) + 'Z' + str(voice_command) + 'F100' + '\n'
         s.write(gcode.encode())
-        print(gcode)
+        #print(gcode)
 
 if __name__ == '__main__':
     port = open_grbl('COM9', 115200)
 
-    send_command(0.5, 0.5, 0.5, port, True)
+    send_command(1, 1, 1, port, True)
 
     close_grbl(port)
