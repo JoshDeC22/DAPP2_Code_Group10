612decf916c40285657ab4e82a9233b89e29956a
maelscott <120776507+maelscott@users.noreply.github.com>
2024-06-10 16:12:24 +0100

    Update grbl_sender.py

--- grbl_sender.py
+++ grbl_sender.py
@@ -1,5 +1,7 @@
 import serial
 import time
+import inverse_kinematics
+import math
 
 def open_grbl(port_name, baudrate=115200):
     s = serial.Serial(port_name, baudrate)
@@ -13,15 +15,20 @@ def close_grbl(serial_port):
     serial_port.reset_input_buffer()
     serial_port.close()
     
-def send_command(voice_command, vertical_gaze_ratio, horizontal_gaze_ratio, s, is_moving):
+def send_command(horizontal_gaze_ratio, vertical_gaze_ratio, voice_command, s, is_moving):
     if is_moving == True:
         gcode = 'G21G91G1' + 'X' + str(horizontal_gaze_ratio) + 'Y' + str(vertical_gaze_ratio) + 'Z' + str(voice_command) + 'F100' + '\n'
         s.write(gcode.encode())
         #print(gcode)
 
 if __name__ == '__main__':
-    port = open_grbl('COM9', 115200)
-
-    send_command(1, 1, 1, port, True)
+    port = open_grbl('COM9')
+    x, y = 0, 420
+    step = inverse_kinematics.inverse_kin_angles(-math.pi / 4)
+    x1 = math.sqrt(3) * 210
+    change = x1 - 420
+    stepX, stepY, new_x, new_y, theta1, theta2 = inverse_kinematics.inverse_kin(x, y, 0, change)
+    print(theta1 * (180 / math.pi), theta2 * (180 / math.pi))
+    send_command(step, 0, 0, port, True)
 
     close_grbl(port)
