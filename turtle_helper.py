b37a8956f0953ffea5b00390f00eb11599e2f5b7
Josh <123667739+JoshDeC22@users.noreply.github.com>
2024-06-12 19:26:42 +0100

    Update turtle_helper.py

--- turtle_helper.py
+++ turtle_helper.py
@@ -10,14 +10,6 @@ def go_right(t):
         t.right(180)
     elif current == 270:
         t.left(90)
-    elif current == 45:
-        t.right(45)
-    elif current == 135:
-        t.right(135)
-    elif current == 225:
-        t.left(135)
-    elif current == 315:
-        t.left(45)
 
 def go_left(t):
     current = t.heading()
@@ -29,14 +21,6 @@ def go_left(t):
         t.left(90)
     elif current == 270:
         t.right(90)
-    elif current == 45:
-        t.left(135)
-    elif current == 135:
-        t.left(45)
-    elif current == 225:
-        t.right(45)
-    elif current == 315:
-        t.right(135)
 
 def go_up(t):
     current = t.heading()
@@ -48,14 +32,6 @@ def go_up(t):
         t.right(90)
     elif current == 270:
         t.right(180)
-    elif current == 45:
-        t.left(45)
-    elif current == 135:
-        t.right(45)
-    elif current == 225:
-        t.right(135)
-    elif current == 315:
-        t.left(135)
 
 def go_down(t):
     current = t.heading()
@@ -67,107 +43,15 @@ def go_down(t):
         t.left(180)
     elif current == 180:
         t.left(90)
-    elif current == 45:
-        t.right(135)
-    elif current == 135:
-        t.left(135)
-    elif current == 225:
-        t.left(45)
-    elif current == 315:
-        t.right(45)
-
-def go_NE(t):
-    current = t.heading()
-    if current == 45:
-        pass
-    elif current == 0:
-        t.left(45)
-    elif current == 90:
-        t.right(45)
-    elif current == 180:
-        t.right(135)
-    elif current == 270:
-        t.left(135)
-    elif current == 135:
-        t.right(90)
-    elif current == 225:
-        t.right(180)
-    elif current == 315:
-        t.left(90)
-
-def go_NW(t):
-    current = t.heading()
-    if current == 135:
-        pass
-    elif current == 0:
-        t.left(135)
-    elif current == 90:
-        t.left(45)
-    elif current == 180:
-        t.right(45)
-    elif current == 270:
-        t.right(135)
-    elif current == 45:
-        t.left(90)
-    elif current == 225:
-        t.right(90)
-    elif current == 315:
-        t.left(180)
-
-def go_SW(t):
-    current = t.heading()
-    if current == 225:
-        pass
-    elif current == 0:
-        t.right(135)
-    elif current == 90:
-        t.left(135)
-    elif current == 180:
-        t.left(45)
-    elif current == 270:
-        t.right(45)
-    elif current == 45:
-        t.right(180)
-    elif current == 135:
-        t.left(90)
-    elif current == 315:
-        t.right(90)
-
-def go_SE(t):
-    current = t.heading()
-    if current == 315:
-        pass
-    elif current == 0:
-        t.right(45)
-    elif current == 90:
-        t.right(135)
-    elif current == 180:
-        t.left(135)
-    elif current == 270:
-        t.left(45)
-    elif current == 45:
-        t.right(90)
-    elif current == 135:
-        t.left(180)
-    elif current == 225:
-        t.left(90)
 
 def move(x, y, t, ismoving):
     if ismoving:
         if x == 1 and y == 0:
             go_right(t)
-        elif x == 1 and y == 1:
-            go_NE(t)
-        elif x == 1 and y == -1:
-            go_SE(t)
         elif x == 0 and y == 1:
             go_up(t)
         elif x == 0 and y == -1:
             go_down(t)
         elif x == -1 and y == 0:
             go_left(t)
-        elif x == -1 and y == 1:
-            go_NW(t)
-        elif x == -1 and y == -1:
-            go_SW(t)
-        t.forward(1)         
+        t.forward(0.5)         
